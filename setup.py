d8a4b53cf32807354f3761a280506f9cb35f3f08
MDM988 <mattdmould@gmail.com>
2023-11-14 14:03:50 -0500

    0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name = 'tqdm_pathos'
-version = '0.3'
+version = '0.4'
 
 with open('README.md' ,'r') as f:
     long_description = f.read().strip()
@@ -20,4 +20,3 @@ setup(
     install_requires=['pathos', 'tqdm'],
     python_requires='>=3.7',
     )
-
